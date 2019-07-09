cfdd4a4dbf961a064dd2ac51a5dbbb09a58255c9
Omer Malik <momer1@live.co.uk>
2019-07-09 12:22:23 +0100

    In this second program I commit “Calculate area of a rectangle” in Java Programming.

---
+++ src/calcarearectangle/CalcAreaRectangle.java
@@ -0,0 +1,18 @@
+package calcarearectangle;
+
+public class CalcAreaRectangle
+{
+    public static void main(String[] args)
+    {
+	// data initialising
+	int length = 23;
+	int breadth = 33;
+		
+	// data processing
+	int area = length * breadth;
+		
+	// output
+	System.out.println ("The area is " + area + " square centimeters.");
+	System.exit(0);
+    }
+}
